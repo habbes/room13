ea1f90c9a2d615ada60306f852a2214b6c9a3f2e
Habbes <haby_habbes@outlook.com>
2015-05-30 06:46:43 +0300

    Created UserEventMessage

---
+++ src/room13/message/messages/UserEventMessage.java
@@ -0,0 +1,42 @@
+package room13.message.messages;
+
+import room13.message.RawMessage;
+
+/**
+ * Base Message for events that notify something about a specific user in a room
+ * @author Habbes
+ *
+ */
+public class UserEventMessage extends EventMessage {
+
+	private String user;
+	
+	public UserEventMessage(String type, String user){
+		super(type);
+		//user is second value param
+		raw.addValue(user); //TODO should it be a Key/Val instead
+	}
+	
+	public UserEventMessage(String type) {
+		this(type, "");
+	}
+
+	public UserEventMessage() {
+		
+	}
+
+	public UserEventMessage(RawMessage rm) {
+		super(rm);
+		user = rm.getValue(1);
+	}
+	
+	public void setUser(String user){
+		this.user = user;
+		raw.setValue(1, user);
+	}
+	
+	public String getUser(){
+		return user;
+	}
+
+}
