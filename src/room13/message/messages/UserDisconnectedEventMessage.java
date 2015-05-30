63ea1cb2b17eebfd513b0dc91c455f2cf7d2f18c
Habbes <haby_habbes@outlook.com>
2015-05-30 10:22:42 +0300

    Created UserDisconnectedEventMessage

---
+++ src/room13/message/messages/UserDisconnectedEventMessage.java
@@ -0,0 +1,24 @@
+package room13.message.messages;
+
+import room13.message.RawMessage;
+
+/**
+ * Event message notifying that a user has been disconnected
+ * @author Habbes
+ *
+ */
+public class UserDisconnectedEventMessage extends UserEventMessage {
+
+	public UserDisconnectedEventMessage(String user) {
+		super(EventMessage.USER_DISCONNECTED);
+	}
+
+	public UserDisconnectedEventMessage() {
+		this("");
+	}
+
+	public UserDisconnectedEventMessage(RawMessage rm) {
+		super(rm);
+	}
+
+}
