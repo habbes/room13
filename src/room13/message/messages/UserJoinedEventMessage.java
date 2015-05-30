b81e64b56cfa1fdc26dfee3fb0294bf740bbd426
Habbes <haby_habbes@outlook.com>
2015-05-30 06:47:01 +0300

    Created UserJoinedEventMessage

---
+++ src/room13/message/messages/UserJoinedEventMessage.java
@@ -0,0 +1,24 @@
+package room13.message.messages;
+
+import room13.message.RawMessage;
+
+/**
+ * Event message notifying that a user has joined the room
+ * @author Habbes
+ *
+ */
+public class UserJoinedEventMessage extends UserEventMessage {
+
+	public UserJoinedEventMessage(String user) {
+		super(EventMessage.USER_JOINED);
+	}
+
+	public UserJoinedEventMessage() {
+		this("");
+	}
+
+	public UserJoinedEventMessage(RawMessage rm) {
+		super(rm);
+	}
+
+}
