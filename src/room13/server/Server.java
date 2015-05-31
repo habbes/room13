452a9c97f28cdb627f4f99d2fa458ae3f1f070db
Habbes <haby_habbes@outlook.com>
2015-05-31 15:04:53 +0300

    Server handles incoming connections

--- src/room13/server/Server.java
+++ src/room13/server/Server.java
@@ -1,20 +1,49 @@
 package room13.server;
 
 import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-import room13.message.*;
-import room13.message.messages.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class Server {
-
-	public Server() {
-		// TODO Auto-generated constructor stub
+	
+	private ServerSocket socket;
+	
+	/**
+	 * used to execute ClientHandlers asynchronously
+	 */
+	private ExecutorService handlerExecutor;
+	
+	private boolean terminateRequested = false;
+	
+	public static final int DEFAULT_PORT = 13500;
+	public static final int DEFAULT_TIMEOUT = 1000;
+	
+	/**
+	 * Creates a server and binds it to the specified port
+	 * @param port
+	 * @throws IOException
+	 */
+	public Server(int port) throws IOException {
+		socket = new ServerSocket(port);
+		socket.setSoTimeout(DEFAULT_TIMEOUT);
+		handlerExecutor = Executors.newCachedThreadPool();
+	}
+	
+	/**
+	 * Creates a server and binds it to the DEFAULT_PORT
+	 * @throws IOException
+	 */
+	public Server() throws IOException{
+		this(DEFAULT_PORT);
 	}
 	
 	List<ClientHandler> clientHandlers = new ArrayList<ClientHandler>();
@@ -51,8 +80,48 @@ public class Server {
 		return room;
 	}
 	
+	/**
+	 * removes a ClientHandler from the server's list
+	 * @param handler
+	 */
 	public void removeClientHandler(ClientHandler handler){
 		clientHandlers.remove(handler);
+	}
+	
+	/**
+	 * add a ClientHandler to the server's list
+	 * @param handler
+	 */
+	public void addClientHandler(ClientHandler handler){
+		clientHandlers.add(handler);
+	}
+	
+	/**
+	 * Request connection handler loop to terminate
+	 */
+	public void requestTerminate(){
+		terminateRequested = true;
+	}
+	
+	/**
+	 * listens for incoming client connections and assign them handlers
+	 */
+	public void handleConnections(){
+		while(!terminateRequested){
+			try{
+				Socket clientSocket = socket.accept();
+				ClientHandler handler = new ClientHandler(new Client(clientSocket), this);
+				addClientHandler(handler);
+				handlerExecutor.submit(handler);
+			}
+			catch(InterruptedIOException e){
+				continue;
+			}
+			catch(IOException e){
+				break;
+			}
+			
+		}
 	}
 	
 
